589617e25a27c3d94a777316d8da45933bdfc3e6
ZoyaaaZoy <rifkyandriansyah444@gmail.com>
2024-08-06 09:06:03 +0700

    Update 06/08/2024 - 09:05

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,42 +1,42 @@
 const Data = [
+    // {
+    //     id: 1,
+    //     title:"Portfolio",
+    //     image: "https://cdn.dribbble.com/userupload/13657292/file/original-f5d4cea4cfb8d8fc56356dfe698de21c.jpg?resize=1024x723",
+    //     link: "https://portfolio-rifkyaa.vercel.app/",
+    // },
     {
         id: 1,
-        title:"Portfolio",
-        image: "https://cdn.dribbble.com/userupload/13657292/file/original-f5d4cea4cfb8d8fc56356dfe698de21c.jpg?resize=1024x723",
-        link: "https://portfolio-rifkyaa.vercel.app/",
-    },
-    {
-        id: 2,
         title:"Mininako",
         image: "https://cdn.dribbble.com/userupload/13657274/file/original-563b03f4b82feb053dd76b414e397156.jpg?resize=752x531",
         link: "https://mininako.vercel.app/",
     },
     {
-        id: 3,
+        id: 2,
         title:"Grand Asritama - Article",
         image: "https://cdn.dribbble.com/userupload/13657244/file/original-d13052714318535a96f2248e88a579ca.jpg?resize=752x531",
         link: "http://www.grandasritama.com/artikel",
     },
     {
-        id: 4,
+        id: 3,
         title:"The Emeralda Resort",
         image: "https://cdn.dribbble.com/userupload/13657228/file/original-41f85b9004186bfd875e7ebe30820f38.jpg?resize=752x531",
         link: "http://ninda-emeralda.rf.gd/",
     },
     {
-        id: 5,
+        id: 4,
         title:"Grand Asritama - Official",
         image: "https://cdn.dribbble.com/userupload/13657209/file/original-263f1cb455b8b6a06d9c605ea446676a.jpg?resize=752x531",
         link: "http://www.grandasritama.com",
     },
     {
-        id: 6,
+        id: 5,
         title:"Grand Asritama - Landing Page",
         image: "https://cdn.dribbble.com/userupload/13657179/file/original-346aa044bb8e2423838b65e34605d295.jpg?resize=752x531",
         link: "http://www.grandasritama.com/gac",
     },
     {
-        id: 7,
+        id: 6,
         title:"Sneakers",
         image: "https://cdn.dribbble.com/userupload/13657196/file/original-858c4130f37df8a2727a5d7a0761010e.jpg?resize=752x531",
         link: "https://dribbble.com/shots/23853673-Sneakers",
